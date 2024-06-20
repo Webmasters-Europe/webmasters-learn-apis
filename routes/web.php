3f7c9297f96b121c6d24eccc9473714d973cc8d2
Christian Eichhorn <c.eichhorn@webmasters.de>
2024-06-20 17:51:19 +0200

    routes fixed

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,18 @@ use Illuminate\Support\Facades\Route;
 Route::view('/documentation', 'documentation.swapi')->domain(config('settings.swapi_url'))->name('documentation.swapi');
 Route::view('/documentation', 'documentation.solar')->domain(config('settings.solar_url'))->name('documentation.solar');
 
+/*Route::domain(config('settings.swapi_url'))->group(function () {
+    Route::view('/documentation', 'documentation.swapi')->name('documentation.swapi');
+});
+Route::domain(config('settings.solar_url'))->group(function () {
+    Route::view('/documentation', 'documentation.solar')->name('documentation.solar');
+})*/;
+
+Route::domain(env('SWAPI_URL'))->get('/documentation', function () {
+    return view('documentation.swapi');
+})->name('documentation.swapi');
+Route::domain(env('SOLAR_URL'))->get('/documentation', function () {
+    return view('documentation.solar');
+})->name('documentation.solar');
+
 Route::view('/', 'welcome');
